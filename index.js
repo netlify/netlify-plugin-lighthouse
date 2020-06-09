d51c0c1e63b36afd021ab4f50853762895e0c97e
erezrokah <erezrokah@users.noreply.github.com>
2020-06-09 21:22:26 +0300

    refactor: use puppeteer chrome

--- index.js
+++ index.js
@@ -1,71 +1,133 @@
-const { resolve } = require('path')
+const httpServer = require("http-server");
+const puppeteer = require("puppeteer");
+const lighthouse = require("lighthouse");
+const chromeLauncher = require("chrome-launcher");
+require("dotenv").config();
 
-const execa = require('execa')
-const chalk = require('chalk')
-const Conf = require('conf') // for simple kv store
+const getServer = (url, serveDir) => {
+  if (url) {
+    console.log(`Scanning url '${url}'`);
+    // return a mock server for readability
+    const server = {
+      listen: async (func) => {
+        await func();
+      },
+      close: () => undefined,
+    };
+    return { server, url };
+  }
 
-// // TODO: enable this in production
-// const NETLIFY_BUILD_BASE = '/opt/buildhome'
+  if (!serveDir) {
+    throw new Error("Empty publish dir");
+  }
+
+  console.log(`Serving and scanning site from directory '${serveDir}'`);
+  const s = httpServer.createServer({ root: serveDir });
+  const port = 5000;
+  const host = "localhost";
+  const server = {
+    listen: (func) => s.listen(port, host, func),
+    close: () => s.close(),
+  };
+  return { url: `http://${host}:${port}`, server };
+};
+
+const belowThreshold = (id, expected, results) => {
+  const category = results.find((c) => c.id === id);
+  if (!category) {
+    console.warn("Could not find category", id);
+  }
+  const actual = category ? category.score : Number.MAX_SAFE_INTEGER;
+  return actual < expected;
+};
+
+const getError = (id, expected, results) => {
+  const category = results.find((c) => c.id === id);
+  return `Expected category '${category.title}' to be greater or equal to '${expected}' but got '${category.score}'`;
+};
 
 module.exports = {
-  name: '@netlify/plugin-lighthouse',
-  // users will be tempted to use semver, but we really don't care
-  onPostDeploy: async ({ pluginConfig }) => {
-    let { site = process.env.SITE, currentVersion, compareWithVersion = 'init' } = pluginConfig
+  onSuccess: async ({
+    constants: { PUBLISH_DIR: serveDir = process.env.PUBLISH_DIR } = {},
+    utils,
+    inputs: {
+      audit_url: auditUrl = process.env.AUDIT_URL,
+      thresholds = process.env.THRESHOLDS || {},
+    } = {},
+  } = {}) => {
+    try {
+      utils = utils || {
+        build: {
+          failBuild: () => {
+            process.exit(1);
+          },
+        },
+        status: {
+          show: () => undefined,
+        },
+      };
 
-    if (typeof currentVersion === `undefined`) {
-      console.log(`lighthouseplugin version not specified, auto assigning ${chalk.yellow("currentVersion='init'")}`)
-      currentVersion = 'init'
-    }
+      if (typeof thresholds === "string") {
+        thresholds = JSON.parse(thresholds);
+      }
 
-    // kvstore in `${NETLIFY_CACHE_DIR}/${name}.json`
-    // we choose to let the user createStore instead of doing it for them
-    // bc they may want to set `defaults` and `schema` and `de/serialize`
-    const store = new Conf({
-      cwd: resolve('cache'),
-      configName: 'netlify-plugin-lighthouse',
-    })
+      const { server, url } = getServer(auditUrl, serveDir);
+      const browserFetcher = puppeteer.createBrowserFetcher();
+      const revisions = await browserFetcher.localRevisions();
+      if (revisions.length <= 0) {
+        throw new Error("Could not find local browser");
+      }
+      const info = await browserFetcher.revisionInfo(revisions[0]);
 
-    // TODO: fetch previous scores from cache
-    await execa('lighthouse-ci', site, { stdio: 'inherit' })
+      const { error, results } = await new Promise((resolve) => {
+        server.listen(async () => {
+          let chrome;
+          try {
+            chrome = await chromeLauncher.launch({
+              chromePath: info.executablePath,
+              chromeFlags: ["--headless", "--no-sandbox", "--disable-gpu"],
+            });
+            const results = await lighthouse(url, {
+              port: chrome.port,
+            });
+            if (results.lhr.runtimeError) {
+              resolve({ error: new Error(results.lhr.runtimeError.message) });
+            }
+            resolve({ error: false, results });
+          } catch (error) {
+            resolve({ error });
+          } finally {
+            if (chrome) {
+              await chrome.kill().catch(() => undefined);
+            }
+            server.close();
+          }
+        });
+      });
+      if (error) {
+        throw error;
+      } else {
+        const categories = Object.values(
+          results.lhr.categories
+        ).map(({ title, score, id }) => ({ title, score, id }));
 
-    // serialize response
-    const curLightHouse = {}
-    const prevLightHouse = store.get(`lighthouse.${compareWithVersion}`)
-    let totalImprovement = 0
-    if (prevLightHouse) {
-      console.log(
-        `Comparing lighthouse results from version: ${chalk.yellow(compareWithVersion)} to version: ${chalk.yellow(
-          currentVersion,
-        )}:`,
-      )
-      Object.entries(curLightHouse).forEach(([k, v]) => {
-        const prevV = prevLightHouse[k]
-        if (!prevV) return // we should never get here but just in case lighthouse format changes...
-        const improvement = v - prevV
-        if (improvement < 0) {
-          console.log(`- ${chalk.yellow(k)} ${chalk.magenta('regressed')} from ${prevV} to ${v}`)
-        } else if (improvement > 0) {
-          console.log(`- ${chalk.yellow(k)} ${chalk.green('improved')} from ${prevV} to ${v}`)
+        const errors = Object.entries(thresholds)
+          .filter(([id, expected]) => belowThreshold(id, expected, categories))
+          .map(([id, expected]) => getError(id, expected, categories));
+
+        const summary = JSON.stringify({ results: categories }, null, 2);
+        console.log(summary);
+        utils.status.show({
+          summary,
+        });
+
+        if (errors.length > 0) {
+          throw new Error(errors.join("\n"));
         }
-        // TODO: print out links to give suggestions on what to do! lets see what lighthouse-ci gives us
-        totalImprovement += improvement
-      })
-      if (Math.abs(totalImprovement) > 2) {
-        // some significance bar
-        const color = totalImprovement > 0 ? chalk.green : chalk.magenta
-        console.log(`${chalk.yellow.bold('Total Improvement')}: ${color(totalImprovement)} points!`)
       }
-    } else {
-      if (compareWithVersion) {
-        console.warn(
-          `Warning: you set ${chalk.yellow('compareWithVersion') +
-            '=' +
-            chalk.yellow(compareWithVersion)} but that version was not found in our result storage.`,
-        )
-      }
+    } catch (error) {
+      console.error(`\nError: ${error.message}\n`);
+      utils.build.failBuild(`failed with error: ${error.message}`);
     }
-    store.set(`lighthouse.${currentVersion}`, curLightHouse)
-    console.log(`Saved results as version: ${chalk.yellow(currentVersion)}`)
   },
-}
+};
