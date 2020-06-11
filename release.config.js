ec329bc4bcd7ecdcaf8f1ce156b98e5a597dde4b
erezrokah <erezrokah@users.noreply.github.com>
2020-06-11 11:43:11 +0300

    chore: add semantic release github plugin

--- release.config.js
+++ release.config.js
@@ -3,6 +3,7 @@ module.exports = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
+    '@semantic-release/github',
     [
       '@semantic-release/npm',
       {
