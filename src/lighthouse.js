b2ace7e15ce36084414d96dd0a7abb7a75948b54
Erez Rokah <erezrokah@users.noreply.github.com>
2020-07-13 19:19:02 +0300

    fix: only set env.DEBUG_COLORS='true' for the plugin (#51)

--- src/lighthouse.js
+++ src/lighthouse.js
@@ -1,10 +1,24 @@
-// prevent logger from prefixing a date when running in tty
+// we set DEBUG_COLORS = 'true' to prevent the logger from prefixing a date when running in tty
+// keep the old DEBUG_COLORS value so we can return it to the original value
+let debugColorsSet = false;
+let debugColorsOriginalValue;
+if ('DEBUG_COLORS' in process.env) {
+  debugColorsSet = true;
+  debugColorsOriginalValue = process.env.DEBUG_COLORS;
+}
 process.env.DEBUG_COLORS = 'true';
 
 const puppeteer = require('puppeteer');
 const lighthouse = require('lighthouse');
 const log = require('lighthouse-logger');
 const chromeLauncher = require('chrome-launcher');
+
+// we can return the original value after requiring the dependencies
+if (debugColorsSet) {
+  process.env.DEBUG_COLORS = debugColorsOriginalValue;
+} else {
+  delete process.env.DEBUG_COLORS;
+}
 
 const getBrowserPath = async () => {
   const browserFetcher = puppeteer.createBrowserFetcher();
