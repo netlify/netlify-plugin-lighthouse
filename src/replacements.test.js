56adcb5dc7f3a6858252f0e6e1ca93b2772f4dfc
Jack Brewer <jack@jackbrewer.co.uk>
2022-09-16 13:26:44 -1000

    linting

--- src/replacements.test.js
+++ src/replacements.test.js
@@ -3,7 +3,7 @@ const { enablePostMessageCommunication } = require('./replacements');
 describe('replacements', () => {
   it('should make enablePostMessageCommunication replacement', () => {
     const data = '</div></body></html>';
-    const replacedContent = enablePostMessageCommunication(data)
+    const replacedContent = enablePostMessageCommunication(data);
     expect(replacedContent).toContain('</div><script>');
     expect(replacedContent).toContain('</script></body></html>');
   });
