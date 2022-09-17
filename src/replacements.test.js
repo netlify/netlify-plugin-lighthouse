a99325bc5614f60c5788af3bdb59ba13dc296c16
Jack Brewer <jack@jackbrewer.co.uk>
2022-09-16 14:06:53 -1000

    fix test

--- src/replacements.test.js
+++ src/replacements.test.js
@@ -1,9 +1,9 @@
-const { enablePostMessageCommunication } = require('./replacements');
+const { makeReplacements } = require('./replacements');
 
 describe('replacements', () => {
   it('should make enablePostMessageCommunication replacement', () => {
     const data = '</div></body></html>';
-    const replacedContent = enablePostMessageCommunication(data);
+    const replacedContent = makeReplacements(data);
     expect(replacedContent).toContain('</div><script>');
     expect(replacedContent).toContain('</script></body></html>');
   });
