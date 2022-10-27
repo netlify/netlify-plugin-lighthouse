70257708af5ebb43dadaf1ae6f802c58dc146af8
Suzanne Aitchison <suzanne.aitchison@netlify.com>
2022-10-27 15:27:45 +0100

    add empty options param to createBrowserFetcher

--- src/lighthouse.js
+++ src/lighthouse.js
@@ -21,7 +21,7 @@ if (debugColorsSet) {
 }
 
 const getBrowserPath = async () => {
-  const browserFetcher = puppeteer.createBrowserFetcher();
+  const browserFetcher = puppeteer.createBrowserFetcher({});
   const revisions = await browserFetcher.localRevisions();
   if (revisions.length <= 0) {
     throw new Error('Could not find local browser');
