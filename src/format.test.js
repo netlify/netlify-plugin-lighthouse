f190323a740685b8fdc6715943b369eafb19f03d
Jack Brewer <jack@jackbrewer.co.uk>
2022-10-27 16:07:39 +0100

    linting…

--- src/format.test.js
+++ src/format.test.js
@@ -8,6 +8,7 @@ const {
 // Strip ANSI color codes from strings, as they make CI sad.
 const stripAnsiCodes = (str) =>
   str.replace(
+    // eslint-disable-next-line no-control-regex
     /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g,
     '',
   );
