0abaa943310e2711d5c80f001b0315cb9eaa8e1e
Suzanne Aitchison <suzanne.aitchison@netlify.com>
2023-01-17 16:10:04 +0000

    try config file

--- src/index.js
+++ src/index.js
@@ -220,7 +220,7 @@ const processResults = ({ data, errors }) => {
 module.exports = {
   onPostBuild: async ({ constants, utils, inputs } = {}) => {
     console.log(
-      '------------------------------ YOU ARE RUNNING AGAINST 19.1.0 pointing explicitly at cache path',
+      '------------------------------ YOU ARE RUNNING AGAINST 19.1.0 with a new puppeteer config file',
     );
 
     const { failBuild, show } = getUtils({ utils });
