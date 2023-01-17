54d7e9d21375846103c4d7d002fd8eb3a6f6ec66
Suzanne Aitchison <suzanne.aitchison@netlify.com>
2023-01-17 15:59:33 +0000

    tell puppeteer to download chrome

--- src/index.js
+++ src/index.js
@@ -220,7 +220,7 @@ const processResults = ({ data, errors }) => {
 module.exports = {
   onPostBuild: async ({ constants, utils, inputs } = {}) => {
     console.log(
-      '------------------------------ YOU ARE RUNNING AGAINST 19.1.0 pointing explicitly at cache path',
+      '------------------------------ YOU ARE RUNNING AGAINST 19.1.0 with download',
     );
 
     const { failBuild, show } = getUtils({ utils });
