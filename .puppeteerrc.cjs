4b7587441af92ca5ba4819f566a61ae9e78c39f5
Suzanne Aitchison <suzanne.aitchison@netlify.com>
2023-01-18 11:25:02 +0000

    point config at cache dir

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -5,5 +5,5 @@ const { join } = require('path');
  */
 module.exports = {
   // Changes the cache location for Puppeteer.
-  cacheDirectory: join(__dirname, 'node_modules', 'puppeteer', '.cache'),
+  cacheDirectory: join(__dirname, 'cache'),
 };
