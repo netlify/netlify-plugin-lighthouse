2df0075631e948f0b5bdd3bd5e764499cbdb1afa
Suzanne Aitchison <suzanne.aitchison@netlify.com>
2023-01-24 15:04:26 +0000

    collect coverage report

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,4 +3,5 @@ module.exports = {
   testMatch: ['**/__tests__/**/*.+(ts|js)', '**/?(*.)+(spec|test).+(ts|js)'],
   setupFiles: ['<rootDir>/test/setup.js'],
   transform: {},
+  collectCoverageFrom: ['**/*.js'],
 };
