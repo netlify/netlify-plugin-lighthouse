a218e36e3b8b20605ff2b3a1812f9d943967314a
Suzanne Aitchison <suzanne.aitchison@netlify.com>
2024-10-21 14:22:43 +0100

    add new spec

--- src/lib/get-settings/get-settings.test.js
+++ src/lib/get-settings/get-settings.test.js
@@ -36,6 +36,11 @@ describe('replacements', () => {
     expect(derivedSettings.settings.locale).toEqual('es');
   });
 
+  it('should skip is-crawlable audit when using deployUrl', () => {
+    const derivedSettings = getSettings({}, true);
+    expect(derivedSettings.settings.skipAudits).toEqual(['is-crawlable']);
+  });
+
   it('should error with incorrect syntax for process.env.SETTINGS', () => {
     process.env.SETTINGS = 'not json';
     expect(getSettings).toThrow(/Invalid JSON/);
