af0c60c54f122fdd78bcac450e0e178e6518f425
Suzanne Aitchison <suzanne.aitchison@netlify.com>
2025-05-14 16:14:34 +0100

    cache utils

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
+import { join } from 'path';
+import { homedir } from 'os';
+
 import * as dotenv from 'dotenv';
 
 import runEvent from './lib/run-event/index.js';
 import getUtils from './lib/get-utils/index.js';
 
 dotenv.config();
+
+const puppeteerCacheDir = join(homedir(), '.cache', 'puppeteer');
+
+const onPreBuild = async ({ utils } = {}) => {
+  console.log('Restoring Lighthouse cache...');
+  // Puppeteer relies on a global cache since v19.x, which otherwise would not be persisted in Netlify builds
+  await utils?.cache.restore(puppeteerCacheDir);
+  console.log('Lighthouse cache restored');
+};
+
+const persistCache = async ({ utils } = {}) => {
+  console.log('Persisting Lighthouse cache...');
+  await utils?.cache.save(puppeteerCacheDir);
+  console.log('Lighthouse cache persisted');
+};
 
 export default function lighthousePlugin(inputs) {
   // Run onSuccess by default, unless inputs specify we should fail_deploy_on_score_thresholds
@@ -14,6 +32,8 @@ export default function lighthousePlugin(inputs) {
 
   if (defaultEvent === 'onSuccess') {
     return {
+      onPreBuild,
+      onPostBuild: ({ utils }) => persistCache({ utils }),
       onSuccess: async ({ constants, utils, inputs } = {}) => {
         // Mock the required `utils` functions if running locally
         const { failPlugin, show } = getUtils({ utils });
@@ -29,10 +49,12 @@ export default function lighthousePlugin(inputs) {
     };
   } else {
     return {
+      onPreBuild,
       onPostBuild: async ({ constants, utils, inputs } = {}) => {
         // Mock the required `utils` functions if running locally
         const { failBuild, show } = getUtils({ utils });
 
+        await persistCache({ utils });
         await runEvent({
           event: 'onPostBuild',
           constants,
